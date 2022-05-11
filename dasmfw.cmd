bf2d0d1bf93ebdec541df308c8058c9712d58672
Phillip Eaton <44277038+phillipeaton@users.noreply.github.com>
2022-05-12 01:33:11 +0200

    dasmfw info file tidyup. Move cmd file parameters to info file

--- dasmfw.cmd
+++ dasmfw.cmd
@@ -1,4 +1,4 @@
 @echo off
-REM dasmfw.exe  -dasm=6502  -info="jetpac.nfo"  -out "jetpac.asm"  "jetpac_2000-3fff.bin"
-dasmfw.exe  -dasm=6502  -info="jetpac.nfo"  -out "jetpac.a65"  -offset=2000  "jetpac_2000-3fff.bin"
+REM dasmfw.exe  -dasm=6502  -info="jetpac.nfo"  -out="jetpac.a65"  -offset=2000  "jetpac_2000-3fff.bin"
+dasmfw.exe                  -info="jetpac.nfo"
 pause
