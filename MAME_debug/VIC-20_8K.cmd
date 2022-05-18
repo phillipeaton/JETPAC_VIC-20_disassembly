316f5fa71851c2ffa67e5536e2f79459cf3048c1
Phillip Eaton <44277038+phillipeaton@users.noreply.github.com>
2022-05-18 23:04:06 +0200

    Adds MAME debugger setup, init code disassembly started

---
+++ MAME_debug/VIC-20_8K.cmd
@@ -0,0 +1,4 @@
+REM mame64.exe vic20p  -quik roms\jetpac.prg  -rompath .\roms  -exp 8k  -debug  -debugger_font_size 9            -nomax
+mame64.exe vic20p  -quik roms\jetpac.prg  -rompath .\roms  -exp 8k  -debug  -debugger_font_size 9  -natural  -nomax
+pause
+
