91251166dd076d951d07c93b2bfac44e7467855b
Phillip Eaton <44277038+phillipeaton@users.noreply.github.com>
2022-05-21 02:00:58 +0200

    Minor progress with game select screen, half inverted characters

--- dasmfw.cmd
+++ dasmfw.cmd
@@ -1,4 +1,4 @@
 @echo off
 REM dasmfw.exe  -dasm=6502  -info="jetpac.nfo"  -out="jetpac.a65"  -offset=2000  "jetpac_2000-3fff.bin"
-dasmfw.exe                  -info="jetpac.nfo"
+dasmfw.exe                  -info="nfo_jetpac.nfo"
 pause
