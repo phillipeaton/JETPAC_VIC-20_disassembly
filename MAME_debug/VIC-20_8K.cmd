14bdfd47b797b0f3b348d279b0cfe7f7fdcd7083
Phillip Eaton <44277038+phillipeaton@users.noreply.github.com>
2022-07-02 02:12:13 +0200

    Added Display_String and Display_Character routines

--- MAME_debug/VIC-20_8K.cmd
+++ MAME_debug/VIC-20_8K.cmd
@@ -1,4 +1,5 @@
 REM mame64.exe vic20p  -quik roms\jetpac.prg  -rompath .\roms  -exp 8k  -debug  -debugger_font_size 9            -nomax
-mame64.exe vic20p  -quik roms\jetpac.prg  -rompath .\roms  -exp 8k  -debug  -debugger_font_size 9  -natural  -nomax
+; mame64.exe vic20p  -quik roms\jetpac.prg  -rompath .\roms  -exp 8k  -debug  -debugger_font_size 9  -natural  -nomax
+mame64.exe vic20p  -quik roms\jetpac.prg  -rompath .\roms  -exp 8k  -natural  -nomax
 pause
 
