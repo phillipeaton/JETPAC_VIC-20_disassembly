297a8ff9165d6b8edb9a2145a06004179da29e7f
Phillip Eaton <44277038+phillipeaton@users.noreply.github.com>
2023-01-08 01:50:39 +0100

    New MAME version, added object spawn fuel/valuable

--- MAME_debug/VIC-20_8K.cmd
+++ MAME_debug/VIC-20_8K.cmd
@@ -1,5 +1,5 @@
-REM mame64.exe vic20p  -quik roms\jetpac.prg  -rompath .\roms  -exp 8k  -nomax -natural
-    mame64.exe vic20p  -quik roms\jetpac.prg  -rompath .\roms  -exp 8k  -nomax -natural -debug -debugger_font_size 9
-REM mame64.exe vic20p  -quik roms\jetpac.prg  -rompath .\roms  -exp 8k  -nomax          -debug -debugger_font_size 9
+REM mame.exe vic20p  -quik roms\jetpac.prg  -rompath .\roms  -exp 8k  -nomax -natural
+    mame.exe vic20p  -quik roms\jetpac.prg  -rompath .\roms  -exp 8k  -nomax -natural -debug -debugger_font_size 9
+REM mame.exe vic20p  -quik roms\jetpac.prg  -rompath .\roms  -exp 8k  -nomax          -debug -debugger_font_size 9
 REM pause
 
